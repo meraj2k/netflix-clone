eb143083ec50fadd39f96a5ded3399324e383840
meraj00 <merajbth786@gmail.com>
2021-12-02 08:54:52 +0530

    final commit

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const footer = () => (
+  <footer className="footer">
+    <div className="footer__copyright">
+      &copy; 2021 Made with ❤️ by{" "}
+      <a className="footer__copyright--link" href="#">
+        {" "}
+        meraj ansari
+      </a>
+    </div>
+  </footer>
+);
+
+export default footer;
